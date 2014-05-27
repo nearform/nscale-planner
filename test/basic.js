86b7a6ceb1105494eb292c9b050cc7bd71f3eaf3
Matteo Collina <matteo.collina@gmail.com>
2014-05-27 19:14:34 +0200

    Added three machines tests.

--- test/basic.js
+++ test/basic.js
@@ -167,4 +167,60 @@ describe("white sheet planning", function() {
      , id: machine1.id
    }])
   })
+
+  it("should create a plan that starts three machines, two docker and one ami", function() {
+
+    var machine1 = defineMachine(amiDefinition)
+
+      , machine2 = defineMachine(dockDef, machine1)
+
+      , machine3 = defineMachine(dockDef, machine1)
+
+      , dest = {
+            "name": "single instance"
+          , "namespace": "single instance"
+          , "id": uuid.v1()
+          , "containerdefinitions": [ amiDefinition ]
+          , "topology": {
+                "containers": {}
+            }
+        }
+
+      , plan
+
+   dest.topology.containers[machine1.id] = machine1
+   dest.topology.containers[machine2.id] = machine2
+   dest.topology.containers[machine3.id] = machine3
+
+   plan = planner(origin, dest)
+
+   expect(plan).to.eql([{
+       cmd: "add"
+     , id: machine1.id
+   }, {
+       cmd: "start"
+     , id: machine1.id
+   }, {
+       cmd: "add"
+     , id: machine3.id
+   }, {
+       cmd: "start"
+     , id: machine3.id
+   }, {
+       cmd: "link"
+     , id: machine3.id
+   }, {
+       cmd: "add"
+     , id: machine2.id
+   }, {
+       cmd: "start"
+     , id: machine2.id
+   }, {
+       cmd: "link"
+     , id: machine2.id
+   }, {
+       cmd: "link"
+     , id: machine1.id
+   }])
+  })
 })
