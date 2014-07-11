7712fbedf7b88c7df7ee48325201ef652b17d4a4
Matteo Collina <matteo.collina@gmail.com>
2014-07-11 11:32:30 +0200

    Extracted allParentsId function and added a unit test.

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 
-var TaskPlanner = require('taskplanner')
-  , _           = require('lodash')
-  , assert      = require('assert')
-  , xtend       = require('xtend')
-  , defaults    = {
+var TaskPlanner   = require('taskplanner')
+  , _             = require('lodash')
+  , assert        = require('assert')
+  , xtend         = require('xtend')
+  , allParentsIds = require('./lib/allParentsIds')
+  , defaults      = {
         mode: 'quick'
       , noLinkUnlinkRemove: false
     }
@@ -382,22 +383,6 @@ function generateOriginTasks(planner, origin, opts) {
       , subTasks: [{ cmd: 'nop' }]
     })
   })
-}
-
-function allParentsIds(context, container, parents) {
-  var isLeaf = parents === undefined
-
-  parents = parents || []
-
-  // doing this before pushing skips the root
-  if (!container.containedBy || container.containedBy === container.id)
-    return parents
-
-  if (!isLeaf)
-    parents.push(container.id)
-
-  // let's order them by tree order
-  return allParentsIds(context, context.topology.containers[container.containedBy], parents)
 }
 
 module.exports = planner
---
+++ test/allParentsIds.js
@@ -0,0 +1,44 @@
+
+var allParentsIds = require('../lib/allParentsIds')
+  , expect  = require("must")
+  , fixture = require("./fixture")
+
+describe('allParentsIds helper', function() {
+  var elbDefinition = fixture.elbDefinition
+    , amiDefinition = fixture.amiDefinition
+    , dockDef       = fixture.dockerDefinition
+    , defineMachine = fixture.defineMachine
+    , buildSheet    = fixture.buildSheet
+    , add           = fixture.addToSheet
+
+  it('should not find any parent on a root machine', function() {
+    var machine1    = defineMachine(amiDefinition)
+      , sheet       = buildSheet("start inside another")
+
+    add(sheet, machine1)
+    expect(allParentsIds(sheet, machine1)).to.eql([])
+  })
+
+  it('should exclude the root machine', function() {
+    var root        = defineMachine(elbDefinition)
+      , machine1    = defineMachine(amiDefinition, root)
+      , sheet       = buildSheet("start inside another")
+
+    add(sheet, machine1)
+    add(sheet, root)
+    expect(allParentsIds(sheet, machine1)).to.eql([])
+  })
+
+  it('should list the instances', function() {
+    var root        = defineMachine(elbDefinition)
+      , machine1    = defineMachine(amiDefinition, root)
+      , machine2    = defineMachine(dockDef, machine1)
+      , sheet       = buildSheet("start inside another")
+
+    add(sheet, root)
+    add(sheet, machine1)
+    add(sheet, machine2)
+
+    expect(allParentsIds(sheet, machine2)).to.eql([machine1.id])
+  })
+})
